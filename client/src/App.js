78d6bc4917db383f4ee952c0cd4fe4906dccc10a
unknown <eawest2@gmail.com>
2018-07-19 14:38:06 -0400

    added navbar and some page styling. More work needs to be done on styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import LinkComp from "./components/stateless/link/link"
+import FolderComp from "./components/stateless/folder/folder"
+import NavComp from "./components/stateless/navbar/navbar"
+import UserComp from "./components/stateless/user/user"
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <LinkComp></LinkComp>
+      <div className="bg-dark">
+        <NavComp></NavComp>
+        <UserComp>
+          <FolderComp>
+            <LinkComp></LinkComp>
+          </FolderComp>
+        </UserComp>
+
       </div>
     );
   }
