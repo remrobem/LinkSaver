1c870b5d99ca01a46a3b34d329badefbbd3b33cc
eawest2 <eawest2@gmail.com>
2018-07-23 21:19:03 -0400

    merge final

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { FolderPage } from './components/folder/folder';
-// import { Link } from './components/link';
-// import { HomePage } from './components/home';
+import  Folder  from './components/folder/folder';
 
 class App extends Component {
+  state = {
+    user: "",
+    userID: "",
+    userFolderList: [],
+    activeFolder: "",
+    activeFolderContent: [],
+    activeLinks:[],
+    newFolder:"",
+    newLink:"",
+    newLinkHref:"",
+    searchTerm:""
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  createNewLink = (title, href) => {
+
+
+  };
+
   render() {
     return (
+      <div>
       <Router>
         <Switch>
-          <Route exact path="/" component={FolderPage} />
+          <Route exact path="/" component={Folder} />
         </Switch>
       </Router>
+      </div>
     );
   }
 }
+
+{/* <div className="bg-dark">
+<NavComp></NavComp>
+<UserComp>
+  <FolderComp>
+    <LinkComp></LinkComp>
+  </FolderComp>
+</UserComp>
+
+</div> */}
 
 export default App;
--- client/src/components/folder/folder.js
+++ client/src/components/folder/folder.js
@@ -1,34 +1,28 @@
-import React, { Component } from "react";
-import { folderService } from "../../services/folderService";
+import React from "react";
+import "./folder.css";
 
-export class FolderPage extends Component {
-  state = {
-    folder: {
-      name: "",
-      description: ""
-    },
-    folders: []
-  };
+const Folder = props => (
+    <div className="col-12 py-2 bg-secondary text-white border border-2 border-white mt-4">
 
-  componentDidMount() {
-    this.getfolder();
-  }
+        <div className="row">
+            <span className="ml-2 col-1"></span>
+            <h4 className = "col-6">FOLDER TITLE TEXT</h4>
+            <div className="col-4">
+                <img className="copy-icon m-1" id="copyButton" alt="Copy Link To Clipboard" src="./assets/images/icons/link.png"></img>
+                <img className="delete-icon m-1" id="deleteButton" alt="Delete Link From Folder" src="./assets/images/icons/delete.png"></img>
+            </div>
+        </div>
 
-  getfolder() {
-    folderService
-      .queryForFolders()
-    //   .then(res => this.setState({ folders: res.data }));
-      .then(res => this.setState({ folders: res.data }));
-  }
+        <div className="spacer row col-12 bg-white mx-2"></div>
 
+        <div className="row">
+            <span className="ml-2 col-2"></span>
+            <div className="col-6">
+            {props.children}
+            </div>
+        </div>
 
-  render() {
-    return (
-      <div>
-          {this.state.folders.map(folder => (
-             <div>{folder.name}</div>
-          ))}ç
-      </div>
-    );
-  }
-}
+    </div>
+);
+
+export default Folder;
