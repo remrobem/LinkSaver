27dc227169f51941cfdd80cbf441cf70cfbc60b9
Rob Martin <rob.martin.5758@gmail.com>
2018-08-01 10:38:20 -0400

    format changes

--- client/src/components/link/link.js
+++ client/src/components/link/link.js
@@ -2,28 +2,28 @@ import React from "react";
 import "./link.css";
 
 const LinkComp = props => (
-    <div className="col-12 my-2 py-2 bg-light text-dark border-2 border-white">
-
+    <div className="my-2 py-2 bg-light text-dark border-2 border-white">
+<div className="container-fluid">
         <div className="row">
-            <span className="ml-2 col-2"></span>
+            <span className="ml-2 col-xs-12"></span>
             <a className="link font-weight-bold mt-2 linka" href={props.url} rel="noopener noreferrer" target="_blank">{props.url}</a>
         </div>
 
-        <div className="spacer row col-12 bg-dark mx-2 my-2"></div>
+        <div className="spacer row col-xs-12 bg-dark mx-2 my-2"></div>
 
         <div className="row">
-            <span className="ml-2 col-1"></span>
-            <div className="col-6">
+            <span className="ml-2 col-xs-2"></span>
+            <div className="col-xs-6">
                 <p>
                     {props.description}
                 </p>
             </div>
-            <div className="col-4">
+            <div className="col-xs-4">
                 <img className="copy-icon m-1" id="copyButton" alt="Copy Link To Clipboard" src="./assets/images/icons/link.png"></img>
                 <img onClick={() => props.deleteLink(props.folderID, props.url)} className="delete-icon m-1" id="deleteButton" alt="Delete Link From Folder" src="./assets/images/icons/delete.png"></img>
             </div>
         </div>
-
+</div>
     </div>
 );
 
--- client/src/components/user/user.js
+++ client/src/components/user/user.js
@@ -2,42 +2,59 @@ import React from "react";
 import "./user.css";
 
 const UserComp = props => (
-    <div className="">
-        <div className="container row">
-            <div className="col-2"></div>
-            <div className="bg-dark col-6">
-                {props.children}
-            </div>
-            <div className="col-4">
-                <div className="container mt-3 bg-secondary pt-2 text-white col-10 rounded border border-2 border-white">
-                    <h5>Add a new folder</h5>
-                    <div className="spacer row col-12 bg-white mx-1 my-2"></div>
-                    <div className="form-group">
-                        <label>Title:</label>
-                        <input
-                            type="text"
-                            className="form-control col-8"
-                            name="newFolder"
-                            value={props.newFolder}
-                            onChange={props.handleInputChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Description:</label>
-                        <input
-                            type="text"
-                            className="form-control col-8"
-                            name="newDescription"
-                            value={props.newDescription}
-                            onChange={props.handleInputChange}
-                        />
-                    </div>
-                    <button className="btn btn-success my-2" onClick={() => props.addFolder()}>Add Folder</button>
-                </div>
-            </div>
+  <div className="">
+    <div className="container">
+      <div classNme="row">
+        <div className="col-xs-8">
+          <div className="bg-dark">{props.children}</div>
         </div>
-    </div>
 
+        <div className="col-xs-4">
+          <div className="container-fluid">
+            {/* <div className="row"> */}
+              <div className="col-xs-12 mt-3 bg-secondary pt-2 text-white  rounded border border-2 border-white">
+                <h5>Add a new folder</h5>
+              </div>
+            {/* </div> */}
+
+            {/* <div className="spacer col-xs-12 bg-white mx-1 my-2" /> */}
+
+            <div className="form-group ">
+              <div className="col-xs-8" />
+              <label style={{color: 'white'}}>Title:</label>
+              <input
+                type="text"
+                className="form-control"
+                name="newFolder"
+                value={props.newFolder}
+                onChange={props.handleInputChange}
+              />
+            </div>
+
+            <div className="form-group ">
+              <div className="col-xs-8">
+                <label style={{color: 'white'}} >Description:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="newDescription"
+                  value={props.newDescription}
+                  onChange={props.handleInputChange}
+                />
+              </div>
+            </div>
+
+            <button
+              className="btn btn-success my-2"
+              onClick={() => props.addFolder()}
+            >
+              Add Folder
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default UserComp;
